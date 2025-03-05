f9ae1ef72691f40d0720bf5d426391b85159539d
Kaique Carvalho <kaiquecarvalho479jogos@gmail.com>
2025-03-05 10:57:06 -0300

    Update HyugaUzumaki interface documentation to clarify the purpose of interfaces

--- dados/src/NivelIntermediario/Heranca/HyugaUzumaki.java
+++ dados/src/NivelIntermediario/Heranca/HyugaUzumaki.java
@@ -1,6 +1,6 @@
 package NivelIntermediario.Heranca;
 //Trabalhando com interfaces. Interface é um contrato que a classe que implementa deve seguir... Não é possivel criar um objeto de uma interface.
-//Interface
+//A principal utilidade das interfaces é permitir que classes diferentes implementem o mesmo conjunto de métodos, sem precisar herdar de uma classe comum.
 public interface HyugaUzumaki {
     void AtivarOKarma();
     void AtivarJougan();
